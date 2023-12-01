112cf93a6c302de46202b02b6fadd50d732e32c2
Hutanu David <hutanu.david@yahoo.ro>
2023-12-01 17:05:40 +0200

    Added uart configuration parameters

--- SRC/test_leduri/Uart.c
+++ SRC/test_leduri/Uart.c
@@ -55,7 +55,7 @@ void UART0_Init(uint32_t baud_rate)
 	UART0->C2 &= ~((UART0_C2_RE_MASK) | (UART0_C2_TE_MASK)); 
 	
 	//Configurare Baud Rate
-	uint32_t osr = 15; // Over-Sampling Rate (numarul de esantioane luate per bit-time)
+	//uint32_t osr = 8; // Over-Sampling Rate (numarul de esantioane luate per bit-time)
 	
 	//SBR - vom retine valoarea baud rate-ului calculat pe baza frecventei ceasului de sistem
 	// 	 SBR  -		b16 b15 b14 [b13 b12 b11 b10 b09		b08 b07 b06 b05 b04 b03 b02 b01] &
@@ -63,12 +63,53 @@ void UART0_Init(uint32_t baud_rate)
 	//            0   0   0    b13 b12 b11 b10 b09    0   0   0   0   0   0   0   0 >> 8
 	//   BDH  -   0   0   0    b13 b12 b11 b10 b09
 	//   BDL  -   b08 b07 b06  b05 b04 b03 b02 b01
-	uint16_t sbr = (uint16_t)((DEFAULT_SYSTEM_CLOCK)/(baud_rate * (osr+1)));
-	uint8_t temp = UART0->BDH & ~(UART0_BDH_SBR(0x1F));
-	UART0->BDH = temp | UART0_BDH_SBR(((sbr & 0x1F00)>> 8));
-	UART0->BDL = (uint8_t)(sbr & UART_BDL_SBR_MASK);
-	UART0->C4 |= UART0_C4_OSR(osr);
 	
+//  // calculeaza Baud Rate Divisor(sbr) pe baza osr dat
+//	uint16_t sbr = (uint16_t)((DEFAULT_SYSTEM_CLOCK)/(baud_rate * (osr+1)));
+
+//  // pune in temp valoare lui UART0_BDH cu ultimii 5 biti pe 0 (care reprezinta sbr) 
+//	uint8_t temp = UART0->BDH & ~(UART0_BDH_SBR(0x1F));
+
+//  // seteaza ultimii 5 biti din UART0_BDH cu bitii 9-14 din sbr
+//	UART0->BDH = temp | UART0_BDH_SBR(((sbr & 0x1F00)>> 8));
+
+//  // pune in UART0_BDL ultimii 8 biti din sbr
+//	UART0->BDL = (uint8_t)(sbr & UART_BDL_SBR_MASK);
+
+//  // seteaza ultimii 5 biti din registrul UART0_C4(care tin de osr) cu osr ales
+//	UART0->C4 |= UART0_C4_OSR(osr);
+
+
+
+//-------SET OVER SAMPLING RATE TO 8--------
+uint32_t osr = 8; // Over-Sampling Rate (numarul de esantioane luate per bit-time)
+
+// calculeaza Baud Rate Divisor(sbr) pe baza osr dat
+uint16_t sbr = (uint16_t)((DEFAULT_SYSTEM_CLOCK) / (baud_rate * (osr + 1)));
+
+// extrage bitii high si low din sbr
+uint8_t sbr_high = (uint8_t)((sbr & 0xFF00) >> 8);
+uint8_t sbr_low = (uint8_t)(sbr & 0xFF);
+
+// seteaza UART0_BDH cu bitii high din sbr
+UART0->BDH = UART0_BDH_SBR(sbr_high);
+
+// seteaza UART0_BDL cu bitii low din sbr
+UART0->BDL = sbr_low;
+
+// seteaza UART0_C4 cu osr ales
+UART0->C4 = (UART0->C4 & ~UART0_C4_OSR_MASK) | UART0_C4_OSR(osr);
+//-------END SET OVER SAMPLING RATE TO 8--------
+
+//-------SET RX INVERTED BIT------
+UART0->S2 |= UART0_S2_RXINV_MASK;
+//-------END SET RX INVERTED BIT-----
+
+
+//-------SET DATA FRAME ORDER TO LSB FIRST----------
+// seteaza LSB bit in UART0_S2 (putea fi lasata si default, e lsb first by default)
+  UART0->S2 &= ~UART0_S2_MSBF_MASK;
+//-------END SET DATA FRAME ORDER TO LSB FIRST----------
 	
 	//Setare numarul de biti de date la 8 si fara bit de paritate
 	UART0->C1 = 0;
@@ -79,13 +120,15 @@ void UART0_Init(uint32_t baud_rate)
 	
 	//Activare intreruperi la receptie
 	UART0->C2 |= UART0_C2_RIE(1);
-	
 	UART0->C2 |= ((UART_C2_RE_MASK) | (UART_C2_TE_MASK));
 	
 	NVIC_EnableIRQ(UART0_IRQn);
-	
 }
 
+
+
+
+// aprinde led-ul de pe pinul pin
 void openLed(int pin)
 {
 	if(pin == LED_PIN12)
@@ -105,6 +148,7 @@ void openLed(int pin)
 	}
 }
 
+// stinge led-ul de pe pinul pin
 void closeLed(int pin)
 {
 	if(pin == LED_PIN12)
@@ -124,6 +168,7 @@ void closeLed(int pin)
 	}
 }
 
+// handler de intrerupere la uart0
 void UART0_IRQHandler(void) 
 {
 		if(UART0->S1 & UART0_S1_RDRF_MASK) 
